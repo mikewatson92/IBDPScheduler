337c4abbcc5af49232237e18f13fdce6f5d152af
Mike Watson <watsonmikej92@gmail.com>
2022-12-08 15:06:45 +0900

    Initial Commit

---
+++ NIHS Scheduler/NIHS_SchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NIHS_SchedulerApp.swift
+//  NIHS Scheduler
+//
+//  Created by ワトソン・マイク on 2022/12/08.
+//
+
+import SwiftUI
+
+@main
+struct NIHS_SchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
